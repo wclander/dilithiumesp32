4853c5a01d9e67f68ab8d29bfb899119fff3ffe4
wclander <wclander@rogers.com>
2023-12-12 11:29:13 -0500

    Add Dilithium and move common to new folder

--- main/main.c
+++ main/main.c
@@ -5,42 +5,44 @@
 #define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE
 #include "esp_log.h"
 
-const char*TAG = "kyber_tests";
+const char*TAG = "dilithium_tests";
 
 #include "esp_sleep.h"
 #include "ccomp_timer.h"
 
 void app_main(void)
 {
-    uint8_t pk[PQCLEAN_KYBER512_CLEAN_CRYPTO_PUBLICKEYBYTES];
-    uint8_t sk[PQCLEAN_KYBER512_CLEAN_CRYPTO_SECRETKEYBYTES];
+    uint8_t pk[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_PUBLICKEYBYTES];
+    uint8_t sk[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_SECRETKEYBYTES];
 
-    uint8_t sse[PQCLEAN_KYBER512_CLEAN_CRYPTO_BYTES];
-    uint8_t ct[PQCLEAN_KYBER512_CLEAN_CRYPTO_CIPHERTEXTBYTES];
+    uint8_t sig[PQCLEAN_DILITHIUM5_CLEAN_CRYPTO_BYTES];
+    size_t siglen;
 
-    uint8_t ss[PQCLEAN_KYBER512_CLEAN_CRYPTO_BYTES];
+    const size_t mlen = 13;
+    const uint8_t m = "TEST MESSAGE";
 
     ccomp_timer_start();
 
     // Generate keypair
-    PQCLEAN_KYBER512_CLEAN_crypto_kem_keypair(pk, sk);
+    PQCLEAN_DILITHIUM5_CLEAN_crypto_sign_keypair(pk, sk);
 
     int64_t keygen_time = ccomp_timer_get_time();
 
-    // Create ciphertext and shared secret
-    PQCLEAN_KYBER512_CLEAN_crypto_kem_enc(ct, sse, pk);
+    // Sign message
+    PQCLEAN_DILITHIUM5_CLEAN_crypto_sign_signature(sig, &siglen, m, mlen, sk);
 
-    int64_t encap_time = ccomp_timer_get_time();
+    int64_t sign_time = ccomp_timer_get_time();
     
-    // Decrypt ciphertext to recover secret
-    PQCLEAN_KYBER512_CLEAN_crypto_kem_dec(ss, ct, sk);
+    // Verify Signed message
+    int result = PQCLEAN_DILITHIUM5_CLEAN_crypto_sign_verify(sig, siglen, m, mlen, sk);
 
-    int64_t decap_time = ccomp_timer_stop();
+    int64_t verify_time = ccomp_timer_stop();
 
-    ESP_LOG_BUFFER_HEX("Encap ss: ", sse, sizeof(sse));
-    ESP_LOG_BUFFER_HEX("Decap ss: ", ss, sizeof(ss));
+    ESP_LOG_BUFFER_HEX("Signature: ", sig, siglen);
 
-    ESP_LOGI(TAG, "Keygen: %qdus, Encap: %qdus, Decap: %qdus", keygen_time, encap_time, decap_time);
+    ESP_LOGI(TAG, "Verification Result: %d", result);
+
+    ESP_LOGI(TAG, "Keygen: %qdus, Sign: %qdus, Verify: %qdus", keygen_time, sign_time, verify_time);
 
     esp_deep_sleep_start();
     while(1){};
